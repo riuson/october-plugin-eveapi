fb6d7d028071c4a2779bc53d45ab05583a458bb0
Vladimir <riuson@gmail.com>
2015-03-15 21:32:30 +0500

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,26 @@
+<?php namespace riuson\EveApi;
+
+use System\Classes\PluginBase;
+
+/**
+ * eveapi Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'EveApi',
+            'description' => 'PHP interface for EVE API',
+            'author'      => 'riuson',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+}

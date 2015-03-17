9e4af0f68d7704c3fafd2057cfcaf4603aa2748d
Vladimir <riuson@gmail.com>
2015-03-17 21:20:48 +0500

    Migration test

---
+++ updates/create_eveapi_cache_table.php
@@ -0,0 +1,33 @@
+<?php namespace riuson\EveApi\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateCacheTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::dropIfExists('riuson_eveapi_cache');
+
+        Schema::create('riuson_eveapi_cache', function($table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+            $table->text('uri');
+            $table->datetime('cached');
+            $table->datetime('cachedUntil');
+            $table->longtext('result');
+            $table->text('error');
+
+            //$table->index('uri');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('riuson_eveapi_cache');
+    }
+
+}
